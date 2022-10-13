2b396c5c40f82d08a36119dc800a523ccb0cc38c
Christopher Robles <crobles@nerdwallet.com>
2022-10-12 21:33:15 -0700

    feat: create func to play round

--- rps.js
+++ rps.js
@@ -4,3 +4,21 @@ const getComputerChoice = () => {
   else if (randNum < 2/3) return 'Paper';
   else return 'Scissors';
 };
+
+const play = (playerSelection, computerSelection) => {
+  if (!playerSelection || !computerSelection) return 'Missing selection!';
+
+  const winningHierarchy = ['rock', 'paper', 'scissors'];
+  const playerSelectionFormatted = playerSelection.toLowerCase();
+  const computerSelectionFormatted = computerSelection.toLowerCase();
+  if (!(winningHierarchy.indexOf(playerSelectionFormatted)+1) || !(winningHierarchy.indexOf(computerSelectionFormatted)+1)) return 'Improper selection!';
+  if (playerSelectionFormatted === computerSelectionFormatted) return 'Tie! Bitches!';
+
+  const playerDefeatIndex = (winningHierarchy.indexOf(playerSelectionFormatted)+1) % 3;
+  if (computerSelectionFormatted === winningHierarchy[playerDefeatIndex]) return 'Computer wins! seriouslly....';
+  else return 'Nice u won against a bot LOL';
+}
+
+const playerMove = 'ROCK';
+const computerMove = getComputerChoice();
+console.log(`play(${playerMove}, ${computerMove}): ${play(playerMove, computerMove)}`);
